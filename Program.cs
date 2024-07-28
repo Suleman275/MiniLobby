f8998f2913bc563339c84fc8bdbbf087bad8b71d
Suleman275 <m.suleman275@gmail.com>
2024-07-28 22:19:32 +0300

    added lobby repository service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using MiniLobby.Data;
 using Microsoft.EntityFrameworkCore;
+using MiniLobby.Interfaces;
+using MiniLobby.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ILobbyRepository, LobbyRepository>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
